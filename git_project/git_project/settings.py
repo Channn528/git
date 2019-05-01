eef953f1364c624e1aca08004d96e04038c6914c
chanyoung <kx328816@naver.com>
2019-05-01 11:04:24 +0000

    project

--- git_project/git_project/settings.py
+++ git_project/git_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e20(vo8usxpy#klrt-4!4$o5x=tq9j-b4@6a--wk*tw5w@f-gj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'git_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'git_project', 'static'),
+    
+    ]
